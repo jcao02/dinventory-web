19885b31fc81e4c88c65f1a041b0572c24393ded
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-15 16:22:49 -0400

    add inventory-new and router modules with main components

---
+++ app/src/modules/inventory-new/InventoryNew.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class InventoryNew extends Component {
+  render() {
+    return (
+      <h1> Inventory New </h1>
+    )
+  }
+}
+export default InventoryNew;
---
+++ app/src/modules/inventory-new/index.ts
@@ -0,0 +1,1 @@
+export { default as InventoryNew } from "./InventoryNew";
