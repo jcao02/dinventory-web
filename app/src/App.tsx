3275f9938f88b44f4f9dd82bbb11f13fae573961
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-15 16:38:07 -0400

    remove default app component and styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import { AppRouter } from './modules/router';
 
@@ -8,23 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
         <AppRouter/>
-
       </div>
     );
   }
